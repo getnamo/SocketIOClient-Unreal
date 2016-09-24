30448ccb9991c1d624330af94dfcd19c52a6dff5
getnamo <getnamo@getnamo.com>
2016-09-24 23:40:14 +0100

    add lambda runnable

---
+++ Source/SocketIOClient/Private/SIOLambdaRunnable.cpp
@@ -0,0 +1,96 @@
+#pragma once
+
+#include "ZipUtilityPrivatePCH.h"
+#include "LambdaRunnable.h"
+
+uint64 FLambdaRunnable::ThreadNumber = 0;
+
+FSIOLambdaRunnable::FSIOLambdaRunnable(TFunction< void()> InFunction)
+{
+	FunctionPointer = InFunction;
+	Finished = false;
+	Number = ThreadNumber;
+
+	FString threadStatGroup = FString::Printf(TEXT("FSIOLambdaRunnable%d"), ThreadNumber);
+	Thread = NULL;
+	Thread = FRunnableThread::Create(this, *threadStatGroup, 0, TPri_BelowNormal); //windows default = 8mb for thread, could specify more
+	ThreadNumber++;
+
+	//Runnables.Add(this);
+}
+
+FSIOLambdaRunnable::~FSIOLambdaRunnable()
+{
+	if (Thread == NULL)
+	{
+		delete Thread;
+		Thread = NULL;
+	}
+
+	//Runnables.Remove(this);
+}
+
+//Init
+bool FSIOLambdaRunnable::Init()
+{
+	//UE_LOG(LogClass, Log, TEXT("FLambdaRunnable %d Init"), Number);
+	return true;
+}
+
+//Run
+uint32 FSIOLambdaRunnable::Run()
+{
+	if (FunctionPointer)
+		FunctionPointer();
+
+	//UE_LOG(LogClass, Log, TEXT("FLambdaRunnable %d Run complete"), Number);
+	return 0;
+}
+
+//stop
+void FSIOLambdaRunnable::Stop()
+{
+	Finished = true;
+}
+
+void FSIOLambdaRunnable::Exit()
+{
+	Finished = true;
+	//UE_LOG(LogClass, Log, TEXT("FLambdaRunnable %d Exit"), Number);
+
+	//delete ourselves when we're done
+	delete this;
+}
+
+void FSIOLambdaRunnable::EnsureCompletion()
+{
+	Stop();
+	Thread->WaitForCompletion();
+}
+
+FSIOLambdaRunnable* FSIOLambdaRunnable::RunLambdaOnBackGroundThread(TFunction< void()> InFunction)
+{
+	FSIOLambdaRunnable* Runnable;
+	if (FPlatformProcess::SupportsMultithreading())
+	{
+		Runnable = new FSIOLambdaRunnable(InFunction);
+		//UE_LOG(LogClass, Log, TEXT("FLambdaRunnable RunLambdaBackGroundThread"));
+		return Runnable;
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
+void FSIOLambdaRunnable::ShutdownThreads()
+{
+	/*for (auto Runnable : Runnables)
+	{
+	if (Runnable != nullptr)
+	{
+	delete Runnable;
+	}
+	Runnable = nullptr;
+	}*/
+}
