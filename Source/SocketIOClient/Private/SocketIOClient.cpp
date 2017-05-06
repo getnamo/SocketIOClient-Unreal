53924fcba693c9cdd87b881579cfd8054e81c94e
getnamo <getnamo@gmail.com>
2017-05-07 00:31:33 +0100

    refactor IModuleInterface to include alternative stack pointer method

--- Source/SocketIOClient/Private/SocketIOClient.cpp
+++ Source/SocketIOClient/Private/SocketIOClient.cpp
@@ -1,20 +1,46 @@
-// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
-
 #include "SocketIOClientPrivatePCH.h"
-
-
+#include "SocketIONative.h"
 
 #define LOCTEXT_NAMESPACE "FSocketIOClientModule"
+
+class FSocketIOClientModule : public ISocketIOClientModule
+{
+public:
+	virtual FSocketIONative* NewValidNativePointer() override;
+
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+private:
+	TArray<FSocketIONative*> ModulePointers;
+};
+
 
 void FSocketIOClientModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+
+	ModulePointers.Empty();
 }
 
 void FSocketIOClientModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
+
+	for (auto Pointer : ModulePointers)
+	{
+		delete Pointer;
+	}
+}
+
+FSocketIONative* FSocketIOClientModule::NewValidNativePointer()
+{
+	FSocketIONative* NewPointer = new FSocketIONative;
+	ModulePointers.Add(NewPointer);
+
+	return NewPointer;
 }
 
 #undef LOCTEXT_NAMESPACE
