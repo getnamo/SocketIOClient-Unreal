56a8128c598aeb638f3301b76f844bdc147606d4
getnamo <getnamo@gmail.com>
2019-01-18 22:35:28 -0800

    add modules for loading

---
+++ Source/SocketIO/Private/SocketIOLib.cpp
@@ -0,0 +1,14 @@
+// Copyright 2018-current Getnamo. All Rights Reserved
+
+
+#include "SocketIOLib.h"
+
+
+//struct 
+
+class FSocketIOModule : public ISocketIOModule
+{
+};
+
+
+IMPLEMENT_MODULE(FSocketIOModule, SocketIO)
---
+++ Source/SocketIO/Public/SocketIOLib.h
@@ -0,0 +1,33 @@
+// Copyright 2018-current Getnamo. All Rights Reserved
+
+
+#pragma once
+
+#include "Runtime/Core/Public/Modules/ModuleManager.h"
+
+
+class SOCKETIO_API ISocketIOModule : public IModuleInterface
+{
+public:
+
+	/**
+	* Singleton - like access to this module's interface.  This is just for convenience!
+	* Beware of calling this during the shutdown phase, though.Your module might have been unloaded already.
+	*
+	* @return Returns singleton instance, loading the module on demand if needed
+	*/
+	static inline ISocketIOModule& Get()
+	{
+		return FModuleManager::LoadModuleChecked< ISocketIOModule >("SocketIO");
+	}
+
+	/**
+	* Checks to see if this module is loaded and ready.  It is only valid to call Get() if IsAvailable() returns true.
+	*
+	* @return True if the module is loaded and ready to use
+	*/
+	static inline bool IsAvailable()
+	{
+		return FModuleManager::Get().IsModuleLoaded("SocketIO");
+	}
+};
