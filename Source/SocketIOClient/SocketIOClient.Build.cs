d88c3e1bf4fcc2a81cd8030b31a709fdff2921e2
getnamo <getnamo@gmail.com>
2019-01-20 16:24:56 -0800

    cleanup pass 1

--- Source/SocketIOClient/SocketIOClient.Build.cs
+++ Source/SocketIOClient/SocketIOClient.Build.cs
@@ -30,39 +30,10 @@ namespace UnrealBuildTool.Rules
             if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
             {
                 isLibrarySupported = true;
-
-                //string PlatformString = (Target.Platform == UnrealTargetPlatform.Win64) ? "Win64" : "Win32";
-                //string BoostLibPath = Path.Combine(BoostThirdParty, "Lib");
-                //string SocketLibPath = Path.Combine(SocketIOThirdParty, "Lib");
-				/*if (Target.Platform == UnrealTargetPlatform.Win32)
-				{
-					PublicAdditionalLibraries.Add(Path.Combine(BoostLibPath, PlatformString, "libboost_date_time-vc141-mt-x32-1_66.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(BoostLibPath, PlatformString, "libboost_random-vc141-mt-x32-1_66.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(BoostLibPath, PlatformString, "libboost_system-vc141-mt-x32-1_66.lib"));
-				}
-				else
-				{
-					PublicAdditionalLibraries.Add(Path.Combine(BoostLibPath, PlatformString, "libboost_date_time-vc141-mt-x64-1_66.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(BoostLibPath, PlatformString, "libboost_random-vc141-mt-x64-1_66.lib"));
-					PublicAdditionalLibraries.Add(Path.Combine(BoostLibPath, PlatformString, "libboost_system-vc141-mt-x64-1_66.lib"));
-				}*/
-
-					
-                //PublicAdditionalLibraries.Add(Path.Combine(SocketLibPath, PlatformString, "sioclient.lib"));
-
             }
             else if (Target.Platform == UnrealTargetPlatform.Linux)
             {
                 isLibrarySupported = true;
-
-                //string PlatformString = "Linux";
-                //string BoostLibPath = Path.Combine(BoostThirdParty, "Lib");
-                //string SocketLibPath = Path.Combine(SocketIOThirdParty, "Lib");
-
-                /*PublicAdditionalLibraries.Add(Path.Combine(BoostLibPath, PlatformString, "libboost_date_time-clang39-mt-1_60.a"));
-                PublicAdditionalLibraries.Add(Path.Combine(BoostLibPath, PlatformString, "libboost_random-clang39-mt-1_60.a"));
-                PublicAdditionalLibraries.Add(Path.Combine(BoostLibPath, PlatformString, "libboost_system-clang39-mt-1_60.a"));*/
-                //PublicAdditionalLibraries.Add(Path.Combine(SocketLibPath, PlatformString, "libsioclient.a"));
             }
             else if (Target.Platform == UnrealTargetPlatform.IOS)
             {
@@ -93,17 +64,6 @@ namespace UnrealBuildTool.Rules
             else if (Target.Platform == UnrealTargetPlatform.Android)
             {
                 isLibrarySupported = true;
-
-                //string PlatformString = "Android";
-				//string AndroidPlatform = "arm64-v8a";   
-				//string AndroidPlatform = "armeabi-v7a"; //change to armeabi-v7a if using 32bit
-				//string BoostLibPath = Path.Combine(BoostThirdParty, "Lib");
-                //string SocketLibPath = Path.Combine(SocketIOThirdParty, "Lib");
-
-                /*PublicAdditionalLibraries.Add(Path.Combine(BoostLibPath, PlatformString, AndroidPlatform, "libboost_date_time-clang-mt-a32-1_66.a"));
-                PublicAdditionalLibraries.Add(Path.Combine(BoostLibPath, PlatformString, AndroidPlatform, "libboost_random-clang-mt-a32-1_66.a"));
-                PublicAdditionalLibraries.Add(Path.Combine(BoostLibPath, PlatformString, AndroidPlatform, "libboost_system-clang-mt-a32-1_66.a"));*/
-                //PublicAdditionalLibraries.Add(Path.Combine(SocketLibPath, PlatformString, AndroidPlatform, "libsioclient.a"));
             }
 
             return isLibrarySupported;
@@ -116,7 +76,6 @@ namespace UnrealBuildTool.Rules
 			PublicIncludePaths.AddRange(
                 new string[] {
 					Path.Combine(ModuleDirectory, "Public"),
-                    Path.Combine(SocketIOThirdParty, "Include"),
                     // ... add public include paths required here ...
                 }
                 );
