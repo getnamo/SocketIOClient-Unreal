07d3a5e052d0fd9d34e6ee724a5102ddc8f02d38
getnamo <getnamo@gmail.com>
2019-07-30 16:47:08 -0700

    expose the library function to c++

--- Source/SocketIOClient/Public/SocketIOFunctionLibrary.h
+++ Source/SocketIOClient/Public/SocketIOFunctionLibrary.h
@@ -14,7 +14,9 @@ UCLASS()
 class SOCKETIOCLIENT_API USocketIOFunctionLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
+
+public:
+
 	/** Static function to spawn a component that doesn't attach */
 	UFUNCTION(BlueprintCallable, Category = "SocketIO Client Static", meta = (WorldContext = "WorldContextObject"))
 	static USocketIOClientComponent* ConstructSocketIOComponent(UObject* WorldContextObject);
