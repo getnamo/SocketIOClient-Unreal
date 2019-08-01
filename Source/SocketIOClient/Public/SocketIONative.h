be484fea380cd34b9dd414b9a69b437df2e9246a
getnamo <getnamo@gmail.com>
2019-08-01 11:22:12 -0700

    add overloaded emit with second param as TEXT() supported
    - needed a macro to handle the two possible text types wchar_t and char16_t

--- Source/SocketIOClient/Public/SocketIONative.h
+++ Source/SocketIOClient/Public/SocketIONative.h
@@ -162,6 +162,30 @@ public:
 		TFunction< void(const TArray<TSharedPtr<FJsonValue>>&)> CallbackFunction = nullptr,
 		const FString& Namespace = TEXT("/"));
 
+
+//flexible type allowing TEXT() passed as second param for overloaded emit
+#if !defined(SIO_TEXT_TYPE)
+	#if PLATFORM_TCHAR_IS_CHAR16
+		#define SIO_TEXT_TYPE char16_t*
+	#else
+		#define SIO_TEXT_TYPE wchar_t*
+	#endif
+#endif
+
+	/**
+	* (Overloaded) Emit an event with a string literal message
+	*
+	* @param EventName				Event name
+	* @param StringMessage			Message in string format
+	* @param CallbackFunction		Optional callback TFunction
+	* @param Namespace				Optional Namespace within socket.io
+	*/
+	void Emit(
+		const FString& EventName,
+		const SIO_TEXT_TYPE StringMessage = TEXT(""),
+		TFunction< void(const TArray<TSharedPtr<FJsonValue>>&)> CallbackFunction = nullptr,
+		const FString& Namespace = TEXT("/"));
+
 	/**
 	* (Overloaded) Emit an event with a number (double) message
 	*
