c130c9c9f96e6f35039fe45bdd88d31b8f3ee7d6
getnamo <getnamo@gmail.com>
2019-09-20 14:15:16 -0700

    doc and small refactor of precise timer
    - bump to 1.1.4. Fairly breaking changes, should not get merged before v1.2

--- Source/CoreUtility/Private/CUPreciseTimer.cpp
+++ Source/CoreUtility/Private/CUPreciseTimer.cpp
@@ -1,34 +1,37 @@
 #include "CUPreciseTimer.h"
 #include "Core.h"
 
-//Toggle to enable/disable code
-#define ENABLE_PRECISE_TIMER 1
-
-#if ENABLE_PRECISE_TIMER
+#if ENABLE_CUPRECISE_TIMER
 static TMap<FString, TSharedPtr<FCUPreciseTimer>> FPreciseTimerInternalMap;
 #endif
 
 void FCUPreciseTimer::Tick(const FString& LogMsg /*= TEXT("TimeTaken")*/)
 {
-#if ENABLE_PRECISE_TIMER
+#if ENABLE_CUPRECISE_TIMER
 	TSharedPtr<FCUPreciseTimer> Timer = MakeShareable(new FCUPreciseTimer);
 	FPreciseTimerInternalMap.Add(LogMsg, Timer);
 	Timer->Then = FPlatformTime::Seconds();	//start timer last so we don't measure anything else
 #endif
 }
 
-void FCUPreciseTimer::Tock(const FString& LogMsg /*= TEXT("TimeTaken")*/)
+double FCUPreciseTimer::Tock(const FString& LogMsg /*= TEXT("TimeTaken")*/, bool bShouldLogResult /*= true*/)
 {
-#if ENABLE_PRECISE_TIMER
+#if ENABLE_CUPRECISE_TIMER
 	double Now = FPlatformTime::Seconds();
 	if (!FPreciseTimerInternalMap.Contains(LogMsg))
 	{
 		UE_LOG(LogTemp, Warning, TEXT("FPreciseTimer::Tock error: <%s> no such category ticked."), *LogMsg);
-		return;
+		return 0.0;
 	}
 	TSharedPtr<FCUPreciseTimer> Timer = FPreciseTimerInternalMap[LogMsg];
 	double Elapsed = (Now - Timer->Then) * 1000.0;
-	UE_LOG(LogTemp, Log, TEXT("%s %1.3f ms"), *LogMsg, Elapsed);
+	if (bShouldLogResult)
+	{
+		UE_LOG(LogTemp, Log, TEXT("%s %1.3f ms"), *LogMsg, Elapsed);
+	}
 	FPreciseTimerInternalMap.Remove(LogMsg);
+#else
+	return 0.0;
 #endif
+	return Elapsed;
 }
--- Source/CoreUtility/Public/CUPreciseTimer.h
+++ Source/CoreUtility/Public/CUPreciseTimer.h
@@ -1,12 +1,26 @@
 #pragma once
 
+#include "CoreMinimal.h"
+
+//Toggle to enable/disable timing code logs
+#define ENABLE_CUPRECISE_TIMER 1
+
+/** C++ Utility Timer class. 
+Usage: 
+FCUPreciseTimer::Tick(TEXT("MyMeasurementCategory");
+//Your code
+FCUPreciseTimer::Tock(TEXT("MyMeasurementCategory"); //This will log the time taken in miliseconds
+
+optionally get the result and handle logging manually
+double Elapsed = FCUPreciseTimer::Tock(TEXT("MyMeasurementCategory", false);
+*/
 class FCUPreciseTimer
 {
 public:
 	static void Tick(const FString& LogMsg = TEXT("TimeTaken"));
 
-	//returns time taken in milliseconds (to micro precision)
-	static void Tock(const FString& LogMsg = TEXT("TimeTaken"));
+	//returns time taken in milliseconds (to micro precision). This function will also log the time taken
+	static double Tock(const FString& LogMsg = TEXT("TimeTaken"), bool bShouldLogResult = true);
 
 private:
 	double Then;
