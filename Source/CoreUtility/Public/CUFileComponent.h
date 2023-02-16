5f6aa996b2f905ca31e8d794ae8397cbec22cfcd
getnamo <getnamo@gmail.com>
2023-02-16 13:06:34 -0800

    add deprecation warning

--- Source/CoreUtility/Public/CUFileComponent.h
+++ Source/CoreUtility/Public/CUFileComponent.h
@@ -1,4 +1,4 @@
-// Copyright 2018-current Getnamo. All Rights Reserved
+// Copyright 2018-current Getnamo. All Rights Reservedfrom UCUFileSubsystem. This component will be removed in the next stable version."
 
 
 #pragma once
@@ -16,32 +16,32 @@ class COREUTILITY_API UCUFileComponent : public UActorComponent
 public:
 
 	/** Get the current project contents directory path*/
-	UFUNCTION(BlueprintPure, Category = FileUtility)
+	UFUNCTION(BlueprintPure, Category = FileUtility, meta = (DeprecatedFunction, DeprecationMessage = "UCUFileComponent has been deprecated, Please use the same functions from UCUFileSubsystem. This component will be removed in the next stable version."))
 	FString ProjectContentsDirectory();
 
 	/** Get the current project directory path*/
-	UFUNCTION(BlueprintPure, Category = FileUtility)
+	UFUNCTION(BlueprintPure, Category = FileUtility, meta = (DeprecatedFunction, DeprecationMessage = "UCUFileComponent has been deprecated, Please use the same functions from UCUFileSubsystem. This component will be removed in the next stable version."))
 	FString ProjectDirectory();
 
 	/** Get the current project saved directory path*/
-	UFUNCTION(BlueprintPure, Category = FileUtility)
+	UFUNCTION(BlueprintPure, Category = FileUtility, meta = (DeprecatedFunction, DeprecationMessage = "UCUFileComponent has been deprecated, Please use the same functions from UCUFileSubsystem. This component will be removed in the next stable version."))
 	FString ProjectSavedDirectory();
 
 	/** External storage in android context, otherwise uses project saved directory*/
-	UFUNCTION(BlueprintPure, Category = FileUtility)
+	UFUNCTION(BlueprintPure, Category = FileUtility, meta = (DeprecatedFunction, DeprecationMessage = "UCUFileComponent has been deprecated, Please use the same functions from UCUFileSubsystem. This component will be removed in the next stable version."))
 	FString ExternalSaveDirectory();
 
-	UFUNCTION(BlueprintPure, Category = FileUtility)
+	UFUNCTION(BlueprintPure, Category = FileUtility, meta = (DeprecatedFunction, DeprecationMessage = "UCUFileComponent has been deprecated, Please use the same functions from UCUFileSubsystem. This component will be removed in the next stable version."))
 	void SplitFullPath(const FString& InFullPath, FString& OutDirectory, FString& OutFileName);
 
-	UFUNCTION(BlueprintPure, Category = FileUtility)
+	UFUNCTION(BlueprintPure, Category = FileUtility, meta = (DeprecatedFunction, DeprecationMessage = "UCUFileComponent has been deprecated, Please use the same functions from UCUFileSubsystem. This component will be removed in the next stable version."))
 	void ProjectRelativePath(const FString& InFullPath, FString& OutProjectRelativePath);
 
 	/** Save array of bytes to file at specified directory */
-	UFUNCTION(BlueprintCallable, Category = FileUtility)
+	UFUNCTION(BlueprintCallable, Category = FileUtility, meta = (DeprecatedFunction, DeprecationMessage = "UCUFileComponent has been deprecated, Please use the same functions from UCUFileSubsystem. This component will be removed in the next stable version."))
 	bool SaveBytesToFile(const TArray<uint8>& Bytes, const FString& Directory, const FString& FileName, bool bLogSave = false);
 
 	/** Read array of bytes from file at specified directory */
-	UFUNCTION(BlueprintCallable, Category = FileUtility)
+	UFUNCTION(BlueprintCallable, Category = FileUtility, meta = (DeprecatedFunction, DeprecationMessage = "UCUFileComponent has been deprecated, Please use the same functions from UCUFileSubsystem. This component will be removed in the next stable version."))
 	bool ReadBytesFromFile(const FString& Directory, const FString& FileName, TArray<uint8>& OutBytes);
 };
