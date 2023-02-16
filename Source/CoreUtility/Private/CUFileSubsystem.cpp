10b0ea5198daa614e951035c2a1746cb190922d6
getnamo <getnamo@gmail.com>
2023-02-16 13:01:56 -0800

    fill the core component functions

--- Source/CoreUtility/Private/CUFileSubsystem.cpp
+++ Source/CoreUtility/Private/CUFileSubsystem.cpp
@@ -1,4 +1,11 @@
 #include "CUFileSubsystem.h"
+#include "HAL/PlatformFileManager.h"
+#include "Misc/FileHelper.h"
+#include "Misc/Paths.h"
+
+#if PLATFORM_ANDROID
+#include "Android/AndroidPlatformMisc.h"
+#endif
 
 void UCUFileSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
@@ -9,3 +16,100 @@ void UCUFileSubsystem::Deinitialize()
 {
 
 }
+
+FString UCUFileSubsystem::ProjectContentsDirectory()
+{
+	return FPaths::ProjectContentDir();
+}
+
+FString UCUFileSubsystem::ProjectDirectory()
+{
+	return FPaths::ProjectDir();
+}
+
+FString UCUFileSubsystem::ProjectSavedDirectory()
+{
+	return FPaths::ProjectSavedDir();
+}
+
+FString UCUFileSubsystem::ExternalSaveDirectory()
+{
+#if PLATFORM_ANDROID
+	return  FString(FAndroidMisc::GamePersistentDownloadDir());
+#else
+	return FPaths::ProjectSavedDir();
+#endif
+}
+
+void UCUFileSubsystem::SplitFullPath(const FString& InFullPath, FString& OutDirectory, FString& OutFileName)
+{
+	bool bDidSplit = InFullPath.Split(TEXT("/"), &OutDirectory, &OutFileName, ESearchCase::CaseSensitive, ESearchDir::FromEnd);
+
+	if (!bDidSplit)
+	{
+		//search by backslash
+		InFullPath.Split(TEXT("\\\\"), &OutDirectory, &OutFileName, ESearchCase::CaseSensitive, ESearchDir::FromEnd);
+	}
+}
+
+void UCUFileSubsystem::ProjectRelativePath(const FString& InFullPath, FString& OutProjectRelativePath)
+{
+	const FString PathToProject = ProjectDirectory();
+
+	FString Before, After;
+
+	InFullPath.Split(PathToProject, &Before, &After);
+
+	OutProjectRelativePath = After;
+}
+
+bool UCUFileSubsystem::SaveBytesToFile(const TArray<uint8>& Bytes, const FString& Directory, const FString& FileName, bool bLogSave)
+{
+	//bool AllowOverwriting = false;
+
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+
+	if (PlatformFile.CreateDirectoryTree(*Directory))
+	{
+		FString AbsoluteFilePath;
+
+		// Get absolute file path
+		if (Directory.EndsWith(TEXT("/")))
+		{
+			AbsoluteFilePath = FPaths::ConvertRelativePathToFull(Directory + FileName);
+		}
+		else
+		{
+			AbsoluteFilePath = FPaths::ConvertRelativePathToFull(Directory + "/" + FileName);
+		}
+
+		// Allow overwriting or file doesn't already exist
+		bool bSaveSuccesful = FFileHelper::SaveArrayToFile(Bytes, *AbsoluteFilePath);
+
+		if (bLogSave)
+		{
+			if (bSaveSuccesful)
+			{
+				UE_LOG(LogTemp, Log, TEXT("Saved: %s with %d bytes"), *AbsoluteFilePath, Bytes.Num());
+			}
+			else
+			{
+				UE_LOG(LogTemp, Log, TEXT("Failed to save: %s"), *AbsoluteFilePath);
+			}
+		}
+
+		return bSaveSuccesful;
+	}
+
+	return false;
+}
+
+bool UCUFileSubsystem::ReadBytesFromFile(const FString& Directory, const FString& FileName, TArray<uint8>& OutBytes)
+{
+	// Get absolute file path
+	FString AbsoluteFilePath = Directory + "/" + FileName;
+
+	// Allow overwriting or file doesn't already exist
+	return FFileHelper::LoadFileToArray(OutBytes, *AbsoluteFilePath);
+}
+
--- Source/CoreUtility/Public/CUFileSubsystem.h
+++ Source/CoreUtility/Public/CUFileSubsystem.h
@@ -9,7 +9,38 @@ class COREUTILITY_API UCUFileSubsystem : public UEngineSubsystem
 	GENERATED_BODY()
 
 public:
+
+	/** Get the current project contents directory path*/
+	UFUNCTION(BlueprintPure, Category = FileUtility)
+	FString ProjectContentsDirectory();
+
+	/** Get the current project directory path*/
+	UFUNCTION(BlueprintPure, Category = FileUtility)
+	FString ProjectDirectory();
+
+	/** Get the current project saved directory path*/
+	UFUNCTION(BlueprintPure, Category = FileUtility)
+	FString ProjectSavedDirectory();
+
+	/** External storage in android context, otherwise uses project saved directory*/
+	UFUNCTION(BlueprintPure, Category = FileUtility)
+	FString ExternalSaveDirectory();
+
+	UFUNCTION(BlueprintPure, Category = FileUtility)
+		void SplitFullPath(const FString& InFullPath, FString& OutDirectory, FString& OutFileName);
+
+	UFUNCTION(BlueprintPure, Category = FileUtility)
+	void ProjectRelativePath(const FString& InFullPath, FString& OutProjectRelativePath);
+
+	/** Save array of bytes to file at specified directory */
+	UFUNCTION(BlueprintCallable, Category = FileUtility)
+	bool SaveBytesToFile(const TArray<uint8>& Bytes, const FString& Directory, const FString& FileName, bool bLogSave = false);
+
+	/** Read array of bytes from file at specified directory */
+	UFUNCTION(BlueprintCallable, Category = FileUtility)
+	bool ReadBytesFromFile(const FString& Directory, const FString& FileName, TArray<uint8>& OutBytes);
 	
+	//Lifetime
 	virtual	void Initialize(FSubsystemCollectionBase& Collection) override;
 	virtual void Deinitialize() override;
 };
